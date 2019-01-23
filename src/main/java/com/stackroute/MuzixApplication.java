b0bb601462a7720fd68063fbc7ab52aad848a79f
Neethu <neethubabu.babu0@gmail.com>
2019-01-23 09:41:12 +0530

    Added @value and environment variables

--- src/main/java/com/stackroute/MuzixApplication.java
+++ src/main/java/com/stackroute/MuzixApplication.java
@@ -6,14 +6,36 @@ package com.stackroute;
 import com.stackroute.domain.Muzix;
 import com.stackroute.repository.MuzixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 
+@ComponentScan
 @SpringBootApplication
+
+//property source path to application properties
+@PropertySource("classpath:application.properties")
 public class MuzixApplication implements ApplicationListener<ContextRefreshedEvent>, CommandLineRunner {
+
+	//@value annotations are used to get the values from application.properties
+	@Value("${trackId}")
+	private int trackId;
+
+	@Value("${trackName}")
+	private String trackName;
+
+	@Value("${comment}")
+	private String comment;
+
+	//An environment variable is created to get the values from application.properties
+	@Autowired
+	Environment env;
 
 	@Autowired
 	MuzixRepository muzixRepository;
@@ -24,13 +46,13 @@ public class MuzixApplication implements ApplicationListener<ContextRefreshedEve
 	//Overriden ApplicationListener method
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-		muzixRepository.save(new Muzix(1,"Broken Angel","Arash"));
+		muzixRepository.save(new Muzix(Integer.parseInt(env.getProperty("trackId")),env.getProperty("trackName"),env.getProperty("comment")));
 	}
 
 	//Overriden CommandLineRunner method
 	@Override
 	public void run(String... args) throws Exception {
-		muzixRepository.save(new Muzix(2,"One day","Arash"));
+		muzixRepository.save(new Muzix(trackId,trackName,comment));
 	}
 }
 
