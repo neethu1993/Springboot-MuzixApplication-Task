938cf50bab57bb099b2e0c5c0abbc2994573b429
Neethu <neethubabu.babu0@gmail.com>
2019-01-23 14:20:05 +0530

    added more comments

--- src/main/java/com/stackroute/exceptions/GlobalExceptionController.java
+++ src/main/java/com/stackroute/exceptions/GlobalExceptionController.java
@@ -1,3 +1,6 @@
+/**
+ * Global Exception controller to handle all the raised exceptions
+ */
 package com.stackroute.exceptions;
 
 import org.springframework.http.HttpStatus;
@@ -9,12 +12,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
 
+//Exception handler to handle Muzix not found exceptions
 @ControllerAdvice
 public class GlobalExceptionController {
     @ExceptionHandler(MuzixNotFoundException.class)
     public ResponseEntity handleMuzixNotFoundException(final MuzixNotFoundException e) {
         return new ResponseEntity(e.getMessage(),HttpStatus.CONFLICT);
     }
+
+    //Exception handler to handle Muzix already exists exceptions
     @ExceptionHandler(MuzixAlreadyExistsException.class)
     public ResponseEntity handleMuzixAlreadyExistsException(final MuzixAlreadyExistsException e){
         return new ResponseEntity(e.getMessage(),HttpStatus.CONFLICT);
